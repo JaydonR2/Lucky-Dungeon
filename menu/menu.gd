d0f02f38121ec3c1ac0b7fe21ad55ff861883829
Lily Atton-Doornbos <lrattond@mtu.edu>
2025-03-09 20:06:28 -0400

    fixed more dependencies, added links between scenes

--- menu/menu.gd
+++ menu/menu.gd
@@ -1,11 +1,10 @@
 extends Control
 
-func _ready():
-	$VBoxContainer/StartButton.grab_focus()
+#func _ready():
+	#$VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed() -> void:
-	pass
-	#get_tree().change_scene_to_file("scene path")
+	get_tree().change_scene_to_file("res://game.tscn")
 	
 func _on_options_button_pressed() -> void:
 	pass # Replace with function body.
--- pause_menu.gd
+++ pause_menu.gd
@@ -7,4 +7,4 @@ func _on_resume_pressed() -> void:
 	
 
 func _on_quit_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://menu/menu.tscn")
