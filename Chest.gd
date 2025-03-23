f1b9950c072895aee8d5ec09a43f1c1b7229d8ca
Swaguffin <sjmckeow@mtu.edu>
2025-03-23 15:15:47 -0400

    Add files via upload

---
+++ Chest.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var is_open = false
+@export var loot: Array[String] = ["Gold", "Potion"]
+
+func _on_body_entered(body):
+	if body.name == "Player" and not is_open:
+		is_open = true
+		$AnimationPlayer.play("open")  # Play chest opening animation
+		give_loot(body)
+
+func give_loot(player):
+	for item in loot:
+		player.add_to_inventory(item)  # Implement inventory system
---
+++ Sword.gd
@@ -0,0 +1,3 @@
+extends Node2D
+@export var damage: int = 10
+@export var fire_rate: float = 0.5
