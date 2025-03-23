9905a1345d5300bf1da8162d80de07fef0dda001
ChNathan <chetjmag@gmail.com>
2025-03-23 15:01:26 -0400

    Added Load func

--- pause_menu.gd
+++ pause_menu.gd
@@ -5,6 +5,9 @@ extends Control
 func _on_resume_pressed() -> void:
 	main.pauseMenu()
 	
-
+func _on_load_pressed() -> void:
+	main.load_game()
+	main.pauseMenu()
+	
 func _on_quit_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu/menu.tscn")
