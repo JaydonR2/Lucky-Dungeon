83c6c3dab57730f159e96997f7540b08326a6fe9
JaydonR2 <jaydon.reinke@gmail.com>
2025-03-23 16:51:47 -0400

    Squashed commit of the following:
    commit 3da5b347da166a901b706bd382e7aad2131c0862
    Author: ChNathan <chetjmag@gmail.com>
    Date:   Sun Mar 23 15:39:27 2025 -0400
    
        Added Load and Save
    
    commit f8e8b90f17988bfdb15919932fb1b68c18462ac3
    Merge: 1cdef07 9905a13
    Author: ChNathan <chetjmag@gmail.com>
    Date:   Sun Mar 23 15:35:10 2025 -0400
    
        Merge branch 'ChethanChanges'
    
    commit 1cdef0717472673b979329faaaa69171351a533f
    Merge: b9bacbd 888e923
    Author: Lily Atton-Doornbos <lrattond@mtu.edu>
    Date:   Sun Mar 23 19:28:30 2025 -0400
    
        Merge branch 'main' of https://github.com/JaydonR2/Lucky-Dungeon
    
    commit b9bacbdcbe37612f9e091a80b491be533c47cf06
    Author: Lily Atton-Doornbos <lrattond@mtu.edu>
    Date:   Sun Mar 23 19:28:16 2025 -0400
    
        fixed
    
    commit c491d1a7f83e5b0d46b2b78effd92beeedb407d2
    Author: Lily Atton-Doornbos <lrattond@mtu.edu>
    Date:   Sun Mar 23 19:21:03 2025 -0400
    
        placed items in game
    
    commit 888e92348977a80c136e5bdf18794dbc3a187f20
    Merge: c491d1a 5f434c6
    Author: Connor-TechGit <theconnorpatrick@gmail.com>
    Date:   Sun Mar 23 15:21:13 2025 -0400
    
        Merge pull request #7 from JaydonR2/connor
    
        Fixed some errors
    
    commit 5f434c64aaa85a1c075a98dcad5571f3762d0f9f
    Author: Connor-TechGit <theconnorpatrick@gmail.com>
    Date:   Sun Mar 23 15:20:41 2025 -0400
    
        Fixed some errors
    
    commit f1b9950c072895aee8d5ec09a43f1c1b7229d8ca
    Author: Swaguffin <sjmckeow@mtu.edu>
    Date:   Sun Mar 23 15:15:47 2025 -0400
    
        Add files via upload
    
    commit 976db2cd55aa7aef7bdc6f17f0f12e468396af17
    Author: Swaguffin <sjmckeow@mtu.edu>
    Date:   Sun Mar 23 15:09:07 2025 -0400
    
        Add files via upload
    
    commit 9905a1345d5300bf1da8162d80de07fef0dda001
    Author: ChNathan <chetjmag@gmail.com>
    Date:   Sun Mar 23 15:01:26 2025 -0400
    
        Added Load func

--- pause_menu.gd
+++ pause_menu.gd
@@ -5,7 +5,10 @@ extends Control
 func _on_resume_pressed() -> void:
 	main.pauseMenu()
 	
-
+func _on_load_pressed() -> void:
+	main.load_game()
+	main.pauseMenu()
+	
 func _on_quit_pressed() -> void:
 	main.get_tree().paused = false
 	get_tree().change_scene_to_file("res://menu/menu.tscn")
---
+++ save_icon.gd
@@ -0,0 +1,3 @@
+extends TextureButton
+func _on_save_icon_pressed():
+	get_parent().save_game()
