8c202ce2591eda97a259ca0587b5bf509e4c197e
JaydonR2 <jaydon.reinke@gmail.com>
2025-04-06 13:22:14 -0400

    slime fixes
    small fixes

--- slime_green.gd
+++ slime_green.gd
@@ -1,22 +1,21 @@
 extends CharacterBody2D
 
-var speed = 100
+var speed = 1
 var player_chase = false
 var player = null
+var touching = false
 var health = 1
 @onready var player_health = %Health
 
 func _physics_process(delta):
 	if player_chase:
 		if position.distance_to(player.position) > 10:
-			position += (player.position - position).normalized() * speed * delta
-			$AnimatedSprite2D.play("side_walk")
+			position += (player.position - position).normalized() * speed
+			$AnimatedSprite2D.play("idle")
 			if (player.position.x-position.x)<0:
 				$AnimatedSprite2D.flip_h=true
 			else:
 				$AnimatedSprite2D.flip_h=false
-	else:
-		$AnimatedSprite2D.play("front_idle")
 	move_and_slide()
 
 func _on_detection_area_body_entered(body):
@@ -28,7 +27,12 @@ func _on_detection_area_body_shape_exited(body_rid, body, body_shape_index, loca
 	player_chase = false
 
 func _on_hitbox_body_entered(body):
+	touching = true
 	PlayerHealth.health_tracker = PlayerHealth.health_tracker - 0.5
 	player_health.update_health()
 	await get_tree().create_timer(1.0).timeout
-	_on_hitbox_body_entered(body)
+	if (touching != false):
+		_on_hitbox_body_entered(body)
+
+func _on_hitbox_body_exited(body):
+	touching = false
