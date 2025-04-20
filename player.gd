665026e19ecc24a2f8813e3d38195a609796cab3
Connor-TechGit <theconnorpatrick@gmail.com>
2025-04-20 14:12:53 -0400

    Revert "Merge pull request #18 from JaydonR2/test"
    This reverts commit d6ab995a7510cbf0d26e0705c874152fee04ca76, reversing
    changes made to 0fe2ab39a4d9ad2e193297d77eae2cc4160112c4.

--- player.gd
+++ player.gd
@@ -4,29 +4,12 @@ const SPEED = 300.0
 
 @export var inv: Inv
 @onready var health = %Health
-@onready var _animated_sprite = $AnimatedSprite2D
-var is_attacking = false
-var taking_damage = false
 
 func _ready():
 	add_to_group("player")
 	
-func _process(_delta):
-	# Handle attack animation
-	if Input.is_action_just_pressed("attack") and not is_attacking:
-		is_attacking = true
-		_animated_sprite.play("attack")
-		_animated_sprite.connect("animation_finished", _on_animation_finished)
-	elif not is_attacking:
-		# Default to idle if not moving or attacking
-		_animated_sprite.play("idle")
 
 func _physics_process(delta):
-	if is_attacking:
-		return  # Skip movement while attacking
-	if taking_damage:
-		return
-	
 	var direction = Vector2.ZERO
 	var directionHori = Input.get_axis("ui_left", "ui_right")
 	var directionVert = Input.get_axis("ui_up", "ui_down")
@@ -41,30 +24,15 @@ func _physics_process(delta):
 	# Set the direction based on the prioritized input
 	if directionHori != 0:
 		direction.x = directionHori
-		_animated_sprite.flip_h = directionHori < 0  # Flip sprite based on direction
 	if directionVert != 0:
 		direction.y = directionVert
 
 	velocity = direction * SPEED
 	move_and_slide()
-	
-		
-	#if direction != Vector2.ZERO:
-	#	_animated_sprite.play("walk")
 
-func _on_animation_finished():
-	if _animated_sprite.animation == "attack":
-		is_attacking = false
-		
 func DecreaseHealth():
 	PlayerHealth.health_tracker = PlayerHealth.health_tracker - 0.5
 	health.update_health()
-
-	taking_damage = true
-	$AnimatedSprite2D.hide()
-	$damage.show()
-	$damage.play("damage")
-
 	
 func UpdateHealth():
 	health.update_health()
@@ -78,9 +46,3 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 # roomR1 health test
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
 	DecreaseHealth()
-
-func _on_damage_animation_finished() -> void:
-	$damage.hide()
-	taking_damage = false
-	$AnimatedSprite2D.show()
-
